c7bf1b336013a61e23488e344dae60ec237c2d50
whiffy <bryanzhao168@yahoo.ca>
2026-04-19 15:47:08 -0700

    Added worker and job processing function for worker

--- back_end/modules/workers/processJob.ts
+++ back_end/modules/workers/processJob.ts
@@ -4,38 +4,45 @@ import type { Logger } from "pino";
 
 import type { Product, SearchQuery } from "@plum/types";
 
-import { embedUserInput } from "../embed-user-input/embedUserInput";
-import { queryDatabase } from "../query/queryDatabase";
-import { transformUserInput } from "../transform-user-input/transformUserInput";
+import { embedUserInput } from "../embed/embedUserInput";
+import type { Embeder } from "../embed/embeder";
+import { ErrorProcessingJob } from "../error/error";
+import type { Querier } from "../query/querier";
+import { queryEmbededUserInput } from "../query/queryDatabase";
+import { transformUserInput } from "../transform/transformUserInput";
+import type { Transformer } from "../transform/transformer";
 
 export default async function processJob(
-  id: number,
-  logger: Logger,
-  job: Job<SearchQuery, any, string>,
-  publisher: Redis,
+	id: number,
+	logger: Logger,
+	job: Job<SearchQuery, any, string>,
+	publisher: Redis,
+
+	embeder: Embeder,
+	transformer: Transformer,
+	querier: Querier,
 ) {
-  try {
-    const userInput = job.data.text;
-    const transformedUserInput = await transformUserInput(userInput);
-    const embededUserInput = await embedUserInput(transformedUserInput);
-    const queriedProducts = await queryDatabase(embededUserInput);
-    logger.info(
-      { worker: id, jobId: job.id, resultCount: queriedProducts.length },
-      "job processed",
-    );
-    await cacheAndPublishResults(job.id, publisher, queriedProducts);
-  } catch (error) {
-    logger.error({ err: error }, "error processing job");
-  }
+	try {
+		const userInput = job.data.text;
+		const transformedUserInput = await transformUserInput(transformer, userInput);
+		const embededUserInput = await embedUserInput(embeder, transformedUserInput);
+		const queriedProducts = await queryEmbededUserInput(querier, embededUserInput);
+		logger.info({ worker: id, jobId: job.id, resultCount: queriedProducts.length }, "job processed");
+		await cacheAndPublishResults(job.id, publisher, queriedProducts);
+	} catch (error) {
+		const errorMessage =
+			error instanceof ErrorProcessingJob ? error.message : "unexpected error processing job";
+		logger.error({ err: error }, errorMessage);
+	}
 }
 
 async function cacheAndPublishResults(
-  jobId: string | undefined,
-  publisher: Redis,
-  queriedProducts: Product[],
+	jobId: string | undefined,
+	publisher: Redis,
+	queriedProducts: Product[],
 ) {
-  const jobResultsId = `results:${jobId}`;
-  const serialized = JSON.stringify(queriedProducts);
-  await publisher.set(jobResultsId, serialized, "EX", 300);
-  await publisher.publish(jobResultsId, serialized);
+	const jobResultsId = `results:${jobId}`;
+	const serialized = JSON.stringify(queriedProducts);
+	await publisher.set(jobResultsId, serialized, "EX", 300);
+	await publisher.publish(jobResultsId, serialized);
 }
--- back_end/modules/workers/worker.ts
+++ back_end/modules/workers/worker.ts
@@ -2,17 +2,24 @@ import { Worker } from "bullmq";
 import Redis from "ioredis";
 import pino from "pino";
 
+import { EmbederGoogle } from "../embed/embederGoogle";
+import { QuerierQdrant } from "../query/querierQdrant";
+import { TransformerGoogle } from "../transform/transformerGoogle";
 import processJob from "./processJob";
 
 export default function startWorkers(count: number) {
-  const logger = pino({ name: "search-worker" });
-  for (var i = 0; i < count; i++) {
-    const connection = new Redis({ maxRetriesPerRequest: null });
-    const publisher = new Redis();
-    new Worker(
-      "search-jobs",
-      async (job) => processJob(i + 1, logger, job, publisher),
-      { connection },
-    );
-  }
+	const logger = pino({ name: "search-worker" });
+	const embeder = new EmbederGoogle();
+	const transformer = new TransformerGoogle();
+	const querier = new QuerierQdrant();
+
+	for (var i = 0; i < count; i++) {
+		const connection = new Redis({ maxRetriesPerRequest: null });
+		const publisher = new Redis();
+		new Worker(
+			"search-jobs",
+			async (job) => processJob(i + 1, logger, job, publisher, embeder, transformer, querier),
+			{ connection },
+		);
+	}
 }
