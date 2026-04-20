30f2aeac282daeb53ec9dfd8a04208fa97d13a2c
whiffy <bryanzhao168@yahoo.ca>
2026-04-20 03:42:06 -0700

    Formatted back end

--- back_end/server/start-queue.ts
+++ back_end/server/start-queue.ts
@@ -2,7 +2,7 @@ import { Queue } from "bullmq";
 import Redis from "ioredis";
 
 export default function startQueue() {
-  const connection = new Redis({ maxRetriesPerRequest: null });
-  const searchQueue = new Queue("search-jobs", { connection });
-  return searchQueue;
+	const connection = new Redis({ maxRetriesPerRequest: null });
+	const searchQueue = new Queue("search-jobs", { connection });
+	return searchQueue;
 }
--- back_end/server/start-server.ts
+++ back_end/server/start-server.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 
 export default function startServer() {
-  const server = new Hono();
-  server.use("/*", cors());
-  return server;
+	const server = new Hono();
+	server.use("/*", cors());
+	return server;
 }
