570d12ac17762737a0b22b8194662ca5a36aa9f2
whiffy <bryanzhao168@yahoo.ca>
2026-04-21 00:04:54 -0700

    Added prettier settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,11 @@
 /** @type {import('prettier').Config} */
 const config = {
-  printWidth: 105,
-  useTabs: true,
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
-  importOrder: ["^(react|next)(/.*)?$", "<THIRD_PARTY_MODULES>", "^@plum/(.*)$", "^[./]"],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
+	printWidth: 105,
+	useTabs: true,
+	plugins: ["@trivago/prettier-plugin-sort-imports"],
+	importOrder: ["^(react|next)(/.*)?$", "<THIRD_PARTY_MODULES>", "^@plum/(.*)$", "^[./]"],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
 };
 
 export default config;
