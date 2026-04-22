53b795f69d049f26468a668d27e37ab35d15e354
whiffy <bryanzhao168@yahoo.ca>
2026-04-22 14:52:33 -0700

    Added function to embeder to embed product data

--- back_end/modules/embed/embeder-google.ts
+++ back_end/modules/embed/embeder-google.ts
@@ -27,11 +27,11 @@ export class EmbederGoogle implements Embeder {
 			});
 			const embeddings = result.embeddings;
 			if (!embeddings) {
-				throw new ErrorEmbed("embeddings are empty");
+				throw new ErrorEmbed("embeddings are empty for user input");
 			}
 
 			if (!embeddings[0]?.values) {
-				throw new ErrorEmbed("embedding's values are empty");
+				throw new ErrorEmbed("embedding's values are empty for user input");
 			}
 
 			return embeddings[0].values;
@@ -41,7 +41,27 @@ export class EmbederGoogle implements Embeder {
 		}
 	}
 
-	async embedProductData(productData: Product): Promise<void> {
-		return;
+	async embedProductData(productData: Product): Promise<number[]> {
+		try {
+			const textToEmbed = `${productData.name}. ${productData.description}`;
+			const result = await this.client.models.embedContent({
+				model: MODEL_NAME,
+				contents: textToEmbed,
+				config: { outputDimensionality: VECTOR_OUTPUT_SIZE },
+			});
+			const embeddings = result.embeddings;
+			if (!embeddings) {
+				throw new ErrorEmbed("embeddings are empty for product data");
+			}
+
+			if (!embeddings[0]?.values) {
+				throw new ErrorEmbed("embedding's values are empty for product data");
+			}
+
+			return embeddings[0].values;
+		} catch (error) {
+			if (error instanceof ErrorEmbed) throw error;
+			throw new ErrorEmbed(error);
+		}
 	}
 }
--- back_end/modules/embed/embeder.ts
+++ back_end/modules/embed/embeder.ts
@@ -3,5 +3,5 @@ import type { Product } from "@plum/types";
 export interface Embeder {
 	readonly embederType: string;
 	embedUserInput(userInput: string): Promise<number[]>;
-	embedProductData(productData: Product): void;
+	embedProductData(productData: Product): Promise<number[]>;
 }
