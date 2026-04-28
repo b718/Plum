1f130f02b289fe4125318164824a09df1fb04142
whiffy <bryanzhao168@yahoo.ca>
2026-04-27 19:55:27 -0700

    Added ability to upload products via the storer interface

--- back_end/modules/query-pipeline/storer/storer.ts
+++ back_end/modules/query-pipeline/storer/storer.ts
@@ -3,5 +3,6 @@ import type { Product } from "@plum/types";
 export interface Storer {
 	readonly storerType: string;
 	upload(jobId: string, products: Product[]): Promise<void>;
+	uploadProduct(product: Product): Promise<void>;
 	query(jobId: string): Promise<Product[]>;
 }
