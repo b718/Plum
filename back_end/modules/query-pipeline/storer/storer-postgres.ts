33a77e689589984ca8fb5e20d07b6b08b5de158f
whiffy <bryanzhao168@yahoo.ca>
2026-04-27 23:40:40 -0700

    Added paged result for postgres storer to display on the front-end

--- back_end/modules/query-pipeline/storer/storer-postgres.ts
+++ back_end/modules/query-pipeline/storer/storer-postgres.ts
@@ -1,11 +1,12 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 
+import { PAGINATION_AMOUNT } from "@plum/consts";
 import type { Product } from "@plum/types";
 
 import { PrismaClient } from "../../../generated/prisma/client";
 import { type Logger, getLogger } from "../../../logger";
 import { ErrorStorer } from "../error/error-storer";
-import type { Storer } from "./storer";
+import type { PagedResult, Storer } from "./storer";
 
 export class StorerPostgres implements Storer {
 	readonly storerType = "prisma-postgres";
@@ -66,22 +67,29 @@ export class StorerPostgres implements Storer {
 		}
 	}
 
-	async query(jobId: string): Promise<Product[]> {
+	async query(jobId: string, page: number): Promise<PagedResult> {
 		try {
-			this.logger.info({ storerType: this.storerType, jobId }, "querying product data");
+			this.logger.info({ storerType: this.storerType, jobId, page }, "querying product data");
 			const result = await this.client.jobResult.findUnique({
-				where: { jobId: jobId },
-				include: { products: true },
+				where: { jobId },
+				include: {
+					products: { skip: (page - 1) * PAGINATION_AMOUNT, take: PAGINATION_AMOUNT },
+					_count: { select: { products: true } },
+				},
 			});
 			if (!result) throw new ErrorStorer(new Error(`no results associated with jobId:'${jobId}'`));
+			const totalPages = Math.ceil(result._count.products / PAGINATION_AMOUNT);
 			this.logger.info(
-				{ storerType: this.storerType, count: result.products.length, jobId },
+				{ storerType: this.storerType, count: result.products.length, jobId, page, totalPages },
 				"successfully queried product data",
 			);
-			return result.products as Product[];
+			return { products: result.products as Product[], page, totalPages };
 		} catch (err) {
-			this.logger.error({ storerType: this.storerType, jobId, err }, "failed to query for products");
-			return [];
+			this.logger.error(
+				{ storerType: this.storerType, jobId, page, err },
+				"failed to query for products",
+			);
+			return { products: [], page, totalPages: 0 };
 		}
 	}
 }
--- back_end/modules/query-pipeline/storer/storer.ts
+++ back_end/modules/query-pipeline/storer/storer.ts
@@ -1,8 +1,14 @@
 import type { Product } from "@plum/types";
 
+export interface PagedResult {
+	products: Product[];
+	page: number;
+	totalPages: number;
+}
+
 export interface Storer {
 	readonly storerType: string;
 	upload(jobId: string, products: Product[]): Promise<void>;
 	uploadProduct(product: Product): Promise<void>;
-	query(jobId: string): Promise<Product[]>;
+	query(jobId: string, page: number): Promise<PagedResult>;
 }
